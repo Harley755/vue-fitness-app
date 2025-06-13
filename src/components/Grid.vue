760ddc79677c677bd6921f5a95af95240f5a4ff4
Harley <bricegoudalo@gmail.com>
2025-06-13 12:11:07 +0100

    feat: add Font Awesome icons for workout types in Grid component

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,5 +1,41 @@
 <template>
-  <section class="mt-5">Hello</section>
+  <section class="mt-5">
+    <div class="row d-flex justify-content-between g-4">
+      <div
+        v-for="(workout, index) in Object.keys(program)"
+        :key="index"
+        class="card border-0 shadow-sm col-md-2 mx-2"
+      >
+        <div class="card-body">
+          <!-- Titre et icône sur la même ligne -->
+          <div class="d-flex justify-content-between align-items-center mb-3">
+            <h4 class="card-title fw-semibold mb-0">
+              Day {{ index < 9 ? '0' + (index + 1) : index + 1 }}
+            </h4>
+            <span>
+              <font-awesome-icon
+                :icon="['fas', 'dumbbell']"
+                v-if="index % 3 === 0"
+              />
+              <font-awesome-icon
+                :icon="['fas', 'weight-hanging']"
+                v-if="index % 3 === 1"
+              />
+              <font-awesome-icon
+                :icon="['fas', 'bolt']"
+                v-if="index % 3 === 2"
+              />
+            </span>
+          </div>
+          <div class="d-flex justify-content-between align-items-center mb-2">
+            <p class="card-text text-justify mb-0">
+              {{ workOutTypes[index % 3] }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -8,6 +44,7 @@ export default {
   data() {
     return {
       program: { ...workoutProgram },
+      workOutTypes: ['Push', 'Pull', 'Legs'],
     }
   },
 
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
-createApp(App).mount("#app");
+// Font Awesome imports
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons'
+import { faWeightHanging } from '@fortawesome/free-solid-svg-icons'
+import { faBolt } from '@fortawesome/free-solid-svg-icons'
+
+// Ajoute les icônes nécessaires à la librairie
+library.add(faDumbbell)
+library.add(faWeightHanging)
+library.add(faBolt)
+
+// Crée l'app et enregistre le composant FontAwesomeIcon globalement
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
